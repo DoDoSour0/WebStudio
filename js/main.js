741c4291cd8831c3213bd77cb04596b4ba95472d
DoDoSour0 <dodosour0@gmail.com>
2024-08-06 10:42:33 +0000

    modified:   css/main.css 	modified:   index.html 	modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -6,3 +6,25 @@ document.addEventListener('DOMContentLoaded', () => {
         menu.classList.toggle('active');
     });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    var acc = document.getElementsByClassName('accordion-btn');
+    for (var i = 0; i < acc.length; i++) {
+        acc[i].addEventListener('click', function () {
+            for (var j = 0; j < acc.length; j++) {
+                if (acc[j] !== this) {
+                    acc[j].classList.remove('active');
+                    acc[j].nextElementSibling.style.maxHeight = null;
+                }
+            }            
+            this.classList.toggle('active');
+            var content = this.nextElementSibling;
+
+            if (content.style.maxHeight) {
+                content.style.maxHeight = null;
+            } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+            }
+        });
+    }
+});
